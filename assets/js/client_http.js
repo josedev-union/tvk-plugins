5bf7e0cdcc25144e1b91f31176e57bf2ce802b82
Hugo Roque <hugolnx@gmail.com>
2019-10-03 15:47:45 -0300

    Adding CORS support

--- assets/js/client_http.js
+++ assets/js/client_http.js
@@ -1,12 +1,12 @@
 class ClientHTTP {
-    static post({url, body, headers = {}}) {
+    static post({url, data, headers = {}}) {
         return new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest()
             xhr.open("POST", url, true)
             for (let name in headers) {
                 xhr.setRequestHeader(name, headers[name])
             }
-            xhr.send(body)
+            xhr.send(data)
             const onResponse = () => {
                 const contentType = xhr.getResponseHeader('Content-Type');
                 const isJsonResponse = typeof(contentType) === 'string' && contentType.toLowerCase().includes('json');
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,25 +5,39 @@ import ImageProcessingSolicitation from '../models/image_processing_solicitation
 import * as signer from '../shared/signer'
 import DentistAccessPoint from '../models/dentist_access_point'
 import SolicitationRateLimit from '../models/solicitation_rate_limit';
+import Uri from '../models/uri'
 
 /* GET presigned post */
-router.post('/image_processing_solicitation', async function(req, res) {
-  let origin = req.get('Referer') || req.get('Origin') || req.get('Host')
-  let signature = req.get('Miroweb-ID')
-  let receivedSignature = typeof(signature) === 'string' && signature !== ''
-  let accessPoints = await DentistAccessPoint.allForHost(origin)
-  let access = accessPoints.find((access) => {
-    return signer.verify(req.body, access.secret, signature)
-  })
+router.options('/image_processing_solicitation', (req, res) => {
+  setCors(req, res)
+  res.status(200).send('')
+})
 
-  if (!receivedSignature || access === undefined) {
+router.post('/image_processing_solicitation', async function(req, res) {
+  let data = {}
+  for (let k in req.body) {
+    data[k] = req.body[k]
+  }
+  const referer = req.get('Referer') || req.get('Origin') || req.get('Host')
+  const receivedReferer = typeof(referer) === 'string' && referer !== ''
+  const signature = req.get('Miroweb-ID')
+  const receivedSignature = typeof(signature) === 'string' && signature !== ''
+  let access
+  if (receivedSignature && receivedReferer) {
+    const accessPoints = await DentistAccessPoint.allForHost(referer)
+    access = accessPoints.find((access) => {
+      return signer.verify(data, access.secret, signature)
+    })
+  }
+
+  if (access === undefined) {
     return res.status(403).send('')
   }
 
   const solicitation = ImageProcessingSolicitation.build(Object.assign({
     ip: req.ip,
-    origin: origin
-  }, req.body))
+    origin: referer
+  }, data))
 
   let hasFreeSlot = await SolicitationRateLimit.build().add(solicitation)
   if (!hasFreeSlot) return res.status(403).send('')
@@ -36,6 +50,8 @@ router.post('/image_processing_solicitation', async function(req, res) {
     credentials.requestUrlToGetProcessed,
   ]
   let [_, uploadJson, urlToGetOriginal, urlToGetProcessed] = await Promise.all(tasks)
+
+  setCors(req, res)
   return res.json({
     presignedUpload: uploadJson,
     presignedDownloadOriginal: urlToGetOriginal,
@@ -50,5 +66,14 @@ router.get('/', async (req, res) => {
   let access = (await DentistAccessPoint.allForHost(req.get('Host')))[0]
   res.render('index', {secret: access.secret})
 })
+
+function setCors(req, res) {
+  const referer = req.get('Referer') || req.get('Origin') || req.get('Host')
+  res.set({
+    "Access-Control-Allow-Origin": new Uri(referer).toString({path: false}),
+    "Access-Control-Allow-Methods": "POST",
+    "Access-Control-Allow-Headers": "MIROWEB-ID",
+  })
+}
 
 export default router
