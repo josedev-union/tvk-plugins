f006813b06b85b4ae1893cc3df84bbc1a317a0e0
Hugo Roque <hugolnx@gmail.com>
2020-11-30 16:09:37 -0300

    Seed task

---
+++ scripts/quick_seed.js
@@ -0,0 +1,49 @@
+import {MiroSmilesUser} from '../src/models/database/MiroSmilesUser'
+import {DentistAccessPoint} from '../src/models/database/DentistAccessPoint'
+
+const USER_EMAIL = 'dentrino@fgmail.com'
+const USER_ID = 'dentrino-ID-1kjn1j3asd'
+const USER_ATTRS = {
+  id: USER_ID,
+  email: USER_EMAIL,
+  company: 'Dentrino Tasty',
+  fullName: 'Dentrino Full Name',
+  country: 'Brazil',
+  phone: '21986411271'
+}
+
+const ACCESS_POINT_ATTRS = {
+  userId: USER_ID,
+  directPage: {
+    slug: 'dentrino',
+    disabled: false,
+  },
+  hosts: [
+    'localhost',
+    'localhost:3000',
+  ]
+}
+
+async function main() {
+  let user = await MiroSmilesUser.getByEmail(USER_EMAIL)
+  if (user) {
+    Object.assign(user, USER_ATTRS)
+  } else {
+    user = new MiroSmilesUser(USER_ATTRS)
+  }
+  await user.save()
+  console.log("User ID", user.id)
+  console.log("User:", user)
+
+  let point = await DentistAccessPoint.findOneByUserId(user.id)
+  if (point) {
+    Object.assign(point, ACCESS_POINT_ATTRS)
+  } else {
+    point = DentistAccessPoint.build(ACCESS_POINT_ATTRS)
+  }
+  await point.save()
+  console.log("Access Point ID", point.id)
+  console.log("Access Point:", point)
+}
+
+main()
