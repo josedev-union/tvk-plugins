7394074ed53b5d5a58cdeee11f0c9512960cbc17
Hugo Roque <hugolnx@gmail.com>
2021-12-17 15:26:58 -0500

    Analytics methods fail-safe

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -6,84 +6,149 @@ export const dentrinoAnalytics = new (class {
   }
 
   reportPageLoadStart() {
-    console.log('Analytics: reportPageLoadStart')
-    this.loadStartTime = new Date()
-    //fbase.logEvent('page_view', {
-    //  page_location: location.href,
-    //  page_title: document.title,
-    //})
+    try {
+      //console.log('Analytics: reportPageLoadStart')
+      this.loadStartTime = new Date()
+      //fbase.logEvent('page_view', {
+      //  page_location: location.href,
+      //  page_title: document.title,
+      //})
+    } catch (err) {
+      console.error("Couldn't report page load start")
+      console.error(err)
+    }
   }
 
   reportPageLoadEnd() {
-    console.log('Analytics: reportPageLoadEnd')
-    fbase.logEvent('page_loaded', {
-      page_location: location.href,
-      page_title: document.title,
-      load_time_secs: Math.round((new Date().getTime() - this.loadStartTime.getTime())/1000)
-    })
+    const evtName = 'page_loaded'
+    try {
+      //console.log('Analytics: reportPageLoadEnd')
+      fbase.logEvent(evtName, {
+        page_location: location.href,
+        page_title: document.title,
+        load_time_secs: Math.round((new Date().getTime() - this.loadStartTime.getTime())/1000)
+      })
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 
   reportSimulationSubmitted(photoSizeBytes, photoType) {
-    const photoSizeMb = Math.round(photoSizeBytes / 1024 / 1024)
-    console.log('Analytics: reportSimulationSubmitted')
-    sessionStorage['tasty_simulation_start'] = new Date().getTime()
-    fbase.logEvent('simulation_submitted', {
-      photo_size_mb: photoSizeMb,
-      photo_type: photoType,
-    })
+    const evtName = 'simulation_submitted'
+    try {
+      const photoSizeMb = Math.round(photoSizeBytes / 1024 / 1024)
+      //console.log('Analytics: reportSimulationSubmitted')
+      sessionStorage['tasty_simulation_start'] = new Date().getTime()
+      fbase.logEvent(evtName, {
+        photo_size_mb: photoSizeMb,
+        photo_type: photoType,
+      })
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 
   reportSimulationUploadFinished() {
-    console.log('Analytics: reportSimulationUploadFinished')
-    fbase.logEvent('photo_upload_finished', {
-      duration_secs: Math.round((new Date().getTime() - parseFloat(sessionStorage['tasty_simulation_start']))/1000)
-    })
+    const evtName = 'photo_upload_finished'
+    try {
+      //console.log('Analytics: reportSimulationUploadFinished')
+      fbase.logEvent(evtName, {
+        duration_secs: Math.round((new Date().getTime() - parseFloat(sessionStorage['tasty_simulation_start']))/1000)
+      })
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 
   reportSimulationSuccessful() {
-    console.log('Analytics: reportSimulationSuccessful')
-    fbase.logEvent('simulation_successful', {
-      duration_secs: Math.round((new Date().getTime() - parseFloat(sessionStorage['tasty_simulation_start']))/1000)
-    })
+    const evtName = 'simulation_successful'
+    try {
+      //console.log('Analytics: reportSimulationSuccessful')
+      fbase.logEvent(evtName, {
+        duration_secs: Math.round((new Date().getTime() - parseFloat(sessionStorage['tasty_simulation_start']))/1000)
+      })
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 
   reportFrontendValidationError(errorCode) {
-    console.log('Analytics: reportFrontendValidationError')
-    fbase.logEvent('validation_error_frontend', {
-      error_code: errorCode,
-    })
+    const evtName = 'error_frontend_validation'
+    try {
+      //console.log('Analytics: reportFrontendValidationError')
+      fbase.logEvent(evtName, {
+        error_code: errorCode,
+      })
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 
   reportBackendValidationError(errorCode) {
-    console.log('Analytics: reportBackendValidationError')
-    fbase.logEvent('validation_error_backend', {
-      error_code: errorCode,
-      duration_secs: Math.round((new Date().getTime() - parseFloat(sessionStorage['tasty_simulation_start']))/1000),
-    })
+    const evtName = 'error_backend'
+    try {
+      //console.log('Analytics: reportBackendValidationError')
+      fbase.logEvent(evtName, {
+        error_code: errorCode,
+        duration_secs: Math.round((new Date().getTime() - parseFloat(sessionStorage['tasty_simulation_start']))/1000),
+      })
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 
   reportSimulationFailed(errorCode) {
-    console.log('Analytics: reportSimulationFailed')
-    fbase.logEvent('simulation_failed', {
-      error_code: errorCode,
-      duration_secs: Math.round((new Date().getTime() - parseFloat(sessionStorage['tasty_simulation_start']))/1000),
-    })
-    this.reportBackendValidationError(errorCode)
+    const evtName = 'simulation_failed'
+    try {
+      //console.log('Analytics: reportSimulationFailed')
+      fbase.logEvent(evtName, {
+        error_code: errorCode,
+        duration_secs: Math.round((new Date().getTime() - parseFloat(sessionStorage['tasty_simulation_start']))/1000),
+      })
+      this.reportBackendValidationError(errorCode)
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 
   reportClickStartOver() {
-    console.log('Analytics: reportClickStartOver')
-    fbase.logEvent('click_start_over')
+    const evtName = 'click_start_over'
+    try {
+      //console.log('Analytics: reportClickStartOver')
+      fbase.logEvent(evtName)
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 
   reportClickDownloadOriginal() {
-    console.log('Analytics: reportClickDownloadOriginal')
-    fbase.logEvent('click_download_original')
+    const evtName = 'click_download_original'
+    try {
+      //console.log('Analytics: reportClickDownloadOriginal')
+      fbase.logEvent(evtName)
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 
   reportClickDownloadResult() {
-    console.log('Analytics: reportClickDownloadResult')
-    fbase.logEvent('click_download_result')
+    const evtName = 'click_download_result'
+    try {
+      //console.log('Analytics: reportClickDownloadResult')
+      fbase.logEvent(evtName)
+    } catch (err) {
+      console.error(`Couldn't report pageLoadEnd ${evtName}`)
+      console.error(err)
+    }
   }
 })()
 
