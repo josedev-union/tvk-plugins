366b8ee5a058af034ed3a119336657e73ad02948
Hugo Roque <hugolnx@gmail.com>
2022-08-05 17:21:45 -0300

    Adapting instant_simulations call to the new Client interface

--- src/routes/instant_simulations.js
+++ src/routes/instant_simulations.js
@@ -134,14 +134,19 @@ helpers.asyncCatchError(async (req, res, next) => {
     }
     const client = new QuickSimulationClient()
     const expiresAt = Math.round(nowSecs + env.instSimGiveUpStartTimeout * 1000.0)
-    const mixFactor = fields.synthTransform == 'true' ? null : env.instSimMixFactor
-    const simulation = await client.requestSimulation({
-      photoPath: files.photo.path,
-      expiresAt: expiresAt,
-      mixFactor: mixFactor,
+    const simOpts = {
       brightness: env.instSimBrightness,
       whiten: env.instSimWhiten,
       poisson: env.instSimPoisson,
+      ortho: false,
+    }
+    if (fields.synthTransform !== 'true') {
+      simOpts['mix_factor'] = env.instSimMixFactor
+    }
+    const simulation = await client.requestSimulation({
+      photoPath: files.photo.path,
+      expiresAt: expiresAt,
+      options: simOpts
     })
     if (timeoutManager.hasTimedout()) return
     const { getBeforeUrl, getResultUrl } = await uploadToFirestoreData({
