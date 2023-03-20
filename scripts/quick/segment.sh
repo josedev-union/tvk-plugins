2cb885b05cc8a7e31641b915a9d3f9372ad2412b
josedev-union <josebarato321@gmail.com>
2023-03-20 11:09:37 +0200

    Remove client credentials from the test script

--- scripts/quick/segment.sh
+++ scripts/quick/segment.sh
@@ -1,16 +1,24 @@
-CLIENT_ID=ODMyMDc2MzEwOTA5OGRVW3MvVnFU
-CLIENT_SECRET=abc77ee1fb7e50bfa2ab7e50edb8c2657106543df1cf4449f6d2ccd0ba033640
-API_SECRET=ff5da5e257233200e1c0a902bbce0c3f
-IMAGE_PATH=./tmp/seg.jpg
+if [ -z "$DENTRINO_CLIENT_ID" ]; then
+    echo "Please enter DENTRINO_CLIENT_ID:"
+    read -r USERNAME_INPUT
+    DENTRINO_CLIENT_ID=$USERNAME_INPUT
+fi
+if [ -z "$DENTRINO_CLIENT_SECRET" ]; then
+    echo "Please enter DENTRINO_CLIENT_SECRET:"
+    read -r USERNAME_INPUT
+    DENTRINO_CLIENT_SECRET=$USERNAME_INPUT
+fi
+DENTRINO_API_SECRET=ff5da5e257233200e1c0a902bbce0c3f
+IMAGE_PATH=$(dirname "$0")/seg_input.jpg
 MD5_B64=$(cat $IMAGE_PATH | openssl md5 -binary | base64 -w0)
 MSG=$USER_ID:$MD5_B64
-KEY=$CLIENT_SECRET:$API_SECRET
+KEY=$DENTRINO_CLIENT_SECRET:$DENTRINO_API_SECRET
 SIGNATURE=$(echo -n "$MSG" | openssl dgst -sha256 -hex -hmac $KEY  | sed 's/^.* //')
-AUTHORIZATION_TOKEN=$(echo -n "$CLIENT_ID:$SIGNATURE" | base64 -w0)
+AUTHORIZATION_TOKEN=$(echo -n "$DENTRINO_CLIENT_ID:$SIGNATURE" | base64 -w0)
 
 curl -XPOST \
   -H "Content-Type: multipart/form-data" \
-	-F "imgPhoto=@./tmp/seg.jpg" \
+	-F "imgPhoto=@$IMAGE_PATH" \
 	"http://localhost:3000/api/segment?clientId=ODMyMDc2MzEwOTA5OGRVW3MvVnFU"
 
 
