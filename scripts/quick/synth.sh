fa4a9b00e426e7c81ce786e3221fda8264f77378
josedev-union <josebarato321@gmail.com>
2023-03-23 20:12:33 +0200

    Add synth script

---
+++ scripts/quick/synth.sh
@@ -0,0 +1,33 @@
+if [ -z "$DENTRINO_CLIENT_ID" ]; then
+    echo "Please enter DENTRINO_CLIENT_ID:"
+    read -r USERNAME_INPUT
+    DENTRINO_CLIENT_ID=$USERNAME_INPUT
+fi
+if [ -z "$DENTRINO_CLIENT_SECRET" ]; then
+    echo "Please enter DENTRINO_CLIENT_SECRET:"
+    read -r USERNAME_INPUT
+    DENTRINO_CLIENT_SECRET=$USERNAME_INPUT
+fi
+DENTRINO_API_SECRET=ff5da5e257233200e1c0a902bbce0c3f
+IMAGE_PATH=$(dirname "$0")/synth_input.png
+MD5_B64=$(cat $IMAGE_PATH | openssl md5 -binary | base64 -w0)
+MSG=$USER_ID:$MD5_B64
+KEY=$DENTRINO_CLIENT_SECRET:$DENTRINO_API_SECRET
+SIGNATURE=$(echo -n "$MSG" | openssl dgst -sha256 -hex -hmac $KEY  | sed 's/^.* //')
+AUTHORIZATION_TOKEN=$(echo -n "$DENTRINO_CLIENT_ID:$SIGNATURE" | base64 -w0)
+
+res=$(curl -XPOST \
+  -H "Authorization: Bearer $AUTHORIZATION_TOKEN" \
+  -H "Content-Type: multipart/form-data" \
+	-F "segmap=@$IMAGE_PATH" \
+	"http://localhost:3000/api/synth?clientId=$DENTRINO_CLIENT_ID" | jq .simulation.result)
+echo $res
+echo $res > synth_output.html
+
+
+
+
+# with open("/tmp/segment.png", "wb") as binary_file:
+
+#     # Write bytes to file
+#     binary_file.write(result["result"])
